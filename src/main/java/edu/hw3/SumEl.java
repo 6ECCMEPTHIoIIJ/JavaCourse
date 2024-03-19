75d13b8b856921d625c59abbc63c2ee59433d967
6ECCMPTHIoIIJ <shamraev.alexandr.github@gmail.com>
2024-03-19 19:59:41 +0300

    comment fix

--- src/main/java/edu/hw3/SumEl.java
+++ src/main/java/edu/hw3/SumEl.java
@@ -1,7 +1,14 @@
 package edu.hw3;
 
 public record SumEl(double x, int n) {
-    public static SumEl of(double x, int n) {
+    /**
+     * Factory method to create a new instance of SumEl.
+     *
+     * @param x the x value
+     * @param n the n value
+     * @return a new instance of SumEl
+     */
+    public static SumEl of(final double x, final int n) {
         return new SumEl(x, n);
     }
 }
